196e1031dc4d9564cf902d3432e8bde11232ec93
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-12-22 17:12:00 +0100

    .

--- server/src/api/routes/trpc.router.ts
+++ server/src/api/routes/trpc.router.ts
@@ -1,21 +1,23 @@
-import { Request, Response, Router } from 'express'
-import { renderTrpcPanel } from 'trpc-panel'
-import { getBaseUrl } from '~/helpers/utils'
+import { Router } from 'express'
+
+// import { renderTrpcPanel } from 'trpc-panel'
+// import { getBaseUrl } from '~/helpers/utils'
 import expressRouteTRPC from '~/trpc/expressRouteTRPC'
-import { appRouter } from '~/trpc/routers/_app'
+
+// import { appRouter } from '~/trpc/routers/_app'
 
 const trpcRouter = Router()
 
-trpcRouter.get('/panel', (_: Request, res: Response) => {
-  console.log(`${getBaseUrl()}/api/trpc`)
-  if (process.env.VERCEL_ENV !== 'development') return res.status(404)
-  res.status(200).send(
-    renderTrpcPanel(appRouter, {
-      url: `${getBaseUrl()}/api/trpc`,
-      transformer: 'superjson',
-    })
-  )
-})
+// trpcRouter.get('/panel', (_: Request, res: Response) => {
+//   console.log(`${getBaseUrl()}/api/trpc`)
+//   if (process.env.VERCEL_ENV !== 'development') return res.status(404)
+//   res.status(200).send(
+//     renderTrpcPanel(appRouter, {
+//       url: `${getBaseUrl()}/api/trpc`,
+//       transformer: 'superjson',
+//     })
+//   )
+// })
 
 trpcRouter.all('*', expressRouteTRPC)
 
