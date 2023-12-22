5e1a6500b29198d59a37efaf8dcff74d96defc29
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-12-22 15:23:03 +0100

    .

--- server/index.js
+++ server/index.js
@@ -1,14 +1,2 @@
-const app = require('express')()
-
-app.get('/api', (req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end('Hello! Go to item:')
-})
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params
-  res.end(`Item: ${slug}`)
-})
-
+const app = require('../dist/server/index')
 module.exports = app
