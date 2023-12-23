f8e6d696fc88b7d9b6c0468303dd8c3385dfc82d
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-12-23 04:20:17 +0100

    rm unnecessary index.html copy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { DefinePlugin } = require('webpack')
@@ -57,9 +56,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './../public/index.html'),
-    }),
     new CopyPlugin({
       patterns: [{ from: '../public/assets', to: './assets' }],
     }),
