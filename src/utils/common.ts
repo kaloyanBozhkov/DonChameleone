54847c15d56aa909bbffa9d792c679f4c8a10e6d
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-12-27 16:15:30 +0100

    .

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,7 @@
+import { env } from '@/env'
+
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''
-  if (process.env.NEXT_PUBLIC_DOMAIN) return `https://${process.env.NEXT_PUBLIC_DOMAIN}`
-  return `http://localhost:${process.env.PORT ?? 3000}`
+  if (env.NEXT_PUBLIC_DOMAIN) return `https://${env.NEXT_PUBLIC_DOMAIN}`
+  return 'http://localhost:3000'
 }
