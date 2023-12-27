0a4164690273988e356eaec59cb548b76fb6b78c
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2023-12-27 16:20:40 +0100

    fix url

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,6 +2,9 @@ import { env } from '@/env'
 
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''
-  if (env.NEXT_PUBLIC_DOMAIN) return `https://${env.NEXT_PUBLIC_DOMAIN}`
+  if (env.NEXT_PUBLIC_DOMAIN)
+    return `${env.NEXT_PUBLIC_DOMAIN.includes('https://') ? '' : 'https://'}${
+      env.NEXT_PUBLIC_DOMAIN
+    }`
   return 'http://localhost:3000'
 }
